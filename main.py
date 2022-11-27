ac2449ece1d60aeb174204caf960306af75cbcb5
ezeiyoke cosmas <43332174+coxxy666@users.noreply.github.com>
2022-11-27 16:24:36 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+total = 0
+for number in range (2,101):
+    if number % 2 == 0 :
+        total += number
+print(total)
+
+total2 = 0
+for number in range (2,101, 2):
+    total2 += number
+print(total2)
+
+
+total1 = 0
+for number in range (1,101):
+    total1 += number
+print(total1)
+    
+
+
+
+
+
+
